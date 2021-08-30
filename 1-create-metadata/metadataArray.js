b71e2d02693ade89e495680929a2799dd4375709
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-08-30 11:29:42 +0530

    Create metadataArray.js

---
+++ 1-create-metadata/metadataArray.js
@@ -0,0 +1,25 @@
+metadataValues=[
+    [
+        "NFT1",
+        "value1",
+        "value2",
+        "value3",
+        "value4"
+    ],
+    [
+        "NFT2",
+        "value1",
+        "value2",
+        "value3",
+        "value4"
+    ],
+    [
+        "NFT3",
+        "value1",
+        "value2",
+        "value3",
+        "value4"
+    ]
+]
+
+module.exports= metadataValues;
